3722d56ac0bca92347a14190491546ffd7ce4fff
v.voronova <malavita21@yandex.ru>
2026-05-13 01:53:17 +0300

    Homework Sprint #8

---
+++ app/src/main/java/com/example/playlistmaker/LibraryActivity.kt
@@ -0,0 +1,13 @@
+package com.example.playlistmaker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
